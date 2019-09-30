dd24eaead3b1e7276fa6cb5272301b6474f50579
Valeriya <roslavcevavaleriya1@bk.ru>
2019-09-30 22:39:57 +0300

    6. Личный проект: больше деталей

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var LIKES_MAX = 200;
 var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var AVATAR_MIN = 1;
 var AVATAR_MAX = 6;
+var AVATAR_WIDTH = 35;
+var AVATAR_HEIGHT = 35;
 var COMMENTS_MIN = 0;
 var COMMENTS_MAX = 20;
 var NAMES = ['Артём', 'Иван', 'Василий', 'Петр', 'Мария'];
@@ -23,7 +25,10 @@ var getRandomMessage = function (messages) {
   var message = '';
   for (var i = 0; i < countMessage; i++) {
     message += getRandomElement(messages);
-  };
+    if (i !== countMessage - 1) {
+      message += ' ';
+    }
+  }
 
   return message;
 };
@@ -34,7 +39,7 @@ var getRandomComments = function () {
 
   for (var i = 0; i < numberOfComments; i++) {
     var usersComment = {
-      avatar: 'img/avatar-' + getRandomInterval(AVATAR_MIN, AVATAR_MAX) +'.svg',
+      avatar: 'img/avatar-' + getRandomInterval(AVATAR_MIN, AVATAR_MAX) + '.svg',
       message: getRandomMessage(MESSAGES),
       name: getRandomElement(NAMES)
     };
@@ -52,9 +57,9 @@ var createPhoto = function (i) {
     description: getRandomElement(DESCRIPTIONS),
     likes: getRandomInterval(LIKES_MIN, LIKES_MAX),
     comments: getRandomComments()
-  }
+  };
 
-return photo;
+  return photo;
 };
 
 var createPhotos = function () {
@@ -70,9 +75,9 @@ var renderPhoto = function (photo) {
   var similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var photoElement = similarPhotoTemplate.cloneNode(true);
 
-  photoElement.querySelector('.picture__img').src=photo.url;
-  photoElement.querySelector('.picture__likes').textContent=photo.likes;
-  photoElement.querySelector('.picture__comments').textContent=photo.comments.length;
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
   return photoElement;
 };
@@ -84,9 +89,43 @@ var renderPhotos = function (photos) {
   for (var i = 0; i < PHOTO_COUNT; i++) {
 
     fragment.appendChild(renderPhoto(photos[i]));
-  };
+  }
 
   photoAlbum.appendChild(fragment);
 };
 
-renderPhotos(createPhotos());
+var photos = createPhotos();
+renderPhotos(photos);
+
+var openBigPicture = function () {
+  var bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+  bigPicture.querySelector('.big-picture__img').firstElementChild.src = photos[0].url;
+  bigPicture.querySelector('.likes-count').textContent = photos[0].likes;
+  bigPicture.querySelector('.comments-count').textContent = photos[0].comments.length;
+  bigPicture.querySelector('.social__caption').textContent = photos[0].description;
+
+  var commentsList = document.querySelector('.social__comments');
+
+  for (var i = 0; i < photos[0].comments.length; i++) {
+    var commentItem = document.createElement('li');
+    commentItem.classList.add('social__comment');
+    var commentAvatar = document.createElement('img');
+    commentAvatar.classList.add('social__picture');
+    commentAvatar.src = photos[0].comments[i].avatar;
+    commentAvatar.alt = photos[0].comments[i].name;
+    commentAvatar.width = AVATAR_WIDTH;
+    commentAvatar.height = AVATAR_HEIGHT;
+
+    var commentText = document.createElement('p');
+    commentText.classList.add('social__text');
+    commentText.textContent = photos[0].comments[i].message;
+    commentItem.appendChild(commentAvatar);
+    commentItem.appendChild(commentText);
+    commentsList.appendChild(commentItem);
+  }
+
+  document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  document.querySelector('.comments-loader').classList.add('visually-hidden');
+};
+openBigPicture();
